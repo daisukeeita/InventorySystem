43a877a8ec10455211fc82cd8014fc2416342757
JAVV <vinas.johnandrei@gmail.com>
2025-02-11 13:13:34 +0800

    modified service for logical operation/s of products

--- src/main/java/com/acolyptos/inventory/services/CategoryService.java
+++ src/main/java/com/acolyptos/inventory/services/CategoryService.java
@@ -1,2 +1,39 @@
 
+package com.acolyptos.inventory.services;
 
+import com.acolyptos.inventory.models.Category;
+import com.acolyptos.inventory.repositories.CategoryRepository;
+
+import org.bson.types.ObjectId;
+import java.util.List;
+
+public class CategoryService {
+
+  private final CategoryRepository categoryRepository;
+
+  public CategoryService() {
+    this.categoryRepository = new CategoryRepository();
+  }
+
+  // Create Category
+  public void createCategory(String name) {
+    Category category = new Category(name);
+
+    categoryRepository.insertCategory(category);
+  }
+
+  // List all Categories
+  public List<Category> listCategories() {
+    return categoryRepository.getAllCategories();
+  }
+
+  // Retrieve a category by ID
+  public Category getCategoryByID(ObjectId id) {
+    return categoryRepository.findCategoryByID(id);
+  }
+
+  // Retrieve a category by Name
+  public Category getCategoryByName(String name) {
+    return categoryRepository.findCategoryByName(name);
+  }
+}
