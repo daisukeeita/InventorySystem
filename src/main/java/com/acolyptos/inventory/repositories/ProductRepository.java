ea2c38e8b982d37b7be57ffacb23c4af4e2d3355
JAVV <vinas.johnandrei@gmail.com>
2025-02-14 10:17:03 +0800

    adding specific product retrieval and manipulation of stock per sale

--- src/main/java/com/acolyptos/inventory/repositories/ProductRepository.java
+++ src/main/java/com/acolyptos/inventory/repositories/ProductRepository.java
@@ -61,6 +61,48 @@ public class ProductRepository {
     return products;
   }
 
+  // Get Product By ID
+  public Product getProductByID(ObjectId id) {
+    Document doc = productCollection.find(Filters.eq("_id", id)).first();
+
+    if (doc != null) {
+      Product product = new Product(doc.getString("name"), doc.getObjectId("categoryID"), doc.getObjectId("supplierID"),
+          doc.getDouble("price"), doc.getInteger("stockQuantity"));
+      product.setID(doc.getObjectId("_id"));
+
+      return product;
+    }
+
+    return null;
+  }
+
+  // Get Product by Name
+  public Product getProductByName(String name) {
+    Document doc = productCollection.find(Filters.eq("name", name)).first();
+
+    if (doc != null) {
+      Product product = new Product(doc.getString("name"), doc.getObjectId("categoryID"), doc.getObjectId("supplierID"),
+          doc.getDouble("price"), doc.getInteger("stockQuantity"));
+      product.setID(doc.getObjectId("_id"));
+
+      return product;
+    }
+
+    return null;
+  }
+
+  // Reduce Stock of a product
+  public void reduceProductStock(ObjectId id, int quantity) {
+    productCollection.updateOne(Filters.eq("_id", id),
+        Updates.inc("stockQuantity", -quantity));
+  }
+
+  // Increase Stock of a product
+  public void increaseProductStock(ObjectId id, int quantity) {
+    productCollection.updateOne(Filters.eq("_id", id),
+        Updates.inc("stockQuantity", quantity));
+  }
+
   // Update a Product
   public void updateProduct(ObjectId id, String name, ObjectId categoryID, ObjectId supplierID, Double price,
       Integer stockQuantity) {
